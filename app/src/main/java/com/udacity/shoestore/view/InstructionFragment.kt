5ecdc12901ce4ef021c8f810be3c9fac13d7bb0e
Isse <sanchezclarisse@gmail.com>
2022-11-22 16:11:28 +0800

    add databinding to all layout

--- app/src/main/java/com/udacity/shoestore/view/InstructionFragment.kt
+++ app/src/main/java/com/udacity/shoestore/view/InstructionFragment.kt
@@ -1,18 +1,13 @@
 package com.udacity.shoestore.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentInstructionBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -20,24 +15,13 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class InstructionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var _binding : FragmentInstructionBinding? = null
+
+    private var _binding: FragmentInstructionBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,28 +36,11 @@ class InstructionFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val instructionButton = binding.instructionButton
         instructionButton.setOnClickListener {
-            view.findNavController().navigate(InstructionFragmentDirections.actionInstructionFragmentToShoeList())
+            view.findNavController()
+                .navigate(InstructionFragmentDirections.actionInstructionFragmentToShoeList())
         }
 
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InstructionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InstructionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
